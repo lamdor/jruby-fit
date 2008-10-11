5b160683840ed091a6edb0623b99659b17115002
Luke Amdor <luke@absoluterubbish.net>
2008-10-11 17:38:52 -0500

    Added deps to classpath when running specs

--- src/spec/spec_helper.rb
+++ src/spec/spec_helper.rb
@@ -4,4 +4,8 @@ require 'spec'
 require 'java'
 BASE_DIR = File.join(File.dirname(__FILE__),  '/../../')
 $CLASSPATH << File.join(BASE_DIR, 'target/classes')
-$:.unshift File.join(BASE_DIR, 'src/main/ruby')
+$CLASSPATH << File.join(BASE_DIR, 'src/main/ruby')
+
+Dir[File.join(BASE_DIR, "target/dependency", "*.jar")].each do |jar|
+  require jar
+end
