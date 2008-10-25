99dd9f53e00b61b7955c43cdac405d2a06aab5fb
Luke Amdor <luke@absoluterubbish.net>
2008-10-25 18:42:58 -0500

    Added ability to add custom type converters

--- src/main/ruby/fit/type_converter.rb
+++ src/main/ruby/fit/type_converter.rb
@@ -1,12 +1,30 @@
 module Fit
   class TypeConverter
+    @converter_types = { }
+
+    def self.register(converter, identifier)
+      raise "#{converter} does not have a parse(value) method" unless converter.respond_to?(:parse)
+      @converter_types[identifier] = converter
+    end
+
     def self.convert(value, type)
-      case type
-      when :string
-        value.to_s
-      when :numeric
+      @converter_types[type].parse(value)
+    end
+
+    class NumericConverter
+      def self.parse(value)
         value.to_i
       end
     end
+
+    class StingConverter
+      def self.parse(value)
+        value.to_s
+      end
+    end
+
+    register NumericConverter, :numeric
+    register StingConverter, :string
+    
   end
 end
--- src/spec/fit/type_converter_spec.rb
+++ src/spec/fit/type_converter_spec.rb
@@ -11,4 +11,32 @@ describe Fit::TypeConverter do
     Fit::TypeConverter.convert("3", :string).should == "3"
   end
 
+  it "should be able to register custom converters as long as the convert has a parse method" do
+    class Dollar
+      attr_reader :amount
+      def initialize(amount)
+        @amount = amount
+      end
+    end
+
+    class DollarConverter
+      def self.parse(value)
+        Dollar.new(value)
+      end
+    end
+
+    Fit::TypeConverter.register(DollarConverter, :dollar)
+
+    converted = Fit::TypeConverter.convert("3", :dollar)
+
+    converted.should be_instance_of(Dollar)
+    converted.amount.should == "3"
+  end
+    
+  it "should raise 'BadConverter does not have a parse(value) method' if the converter of course doesn't have it" do
+    class BadConverter
+    end
+    lambda { Fit::TypeConverter.register BadConverter, :somethign }.should raise_error('BadConverter does not have a parse(value) method')
+  end
+
 end
